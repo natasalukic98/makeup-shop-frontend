00c45da75cae0ba8ba3b19f0c07aa047c1fff88f
natasalukic98 <natasa.lukic98.nl@gmail.com>
2023-02-01 15:46:28 +0100

    Add keycloak

--- src/app/components/responsive-toolbar/responsive-toolbar.component.ts
+++ src/app/components/responsive-toolbar/responsive-toolbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-responsive-toolbar',
   templateUrl: './responsive-toolbar.component.html',
   styleUrls: ['./responsive-toolbar.component.css']
 })
-export class ResponsiveToolbarComponent {
+export class ResponsiveToolbarComponent implements OnInit {
+
+  constructor (private keycloakService:KeycloakService){
+    
+  }
+  ngOnInit(): void {
+  }
+  logout (): void {
+    this.keycloakService.logout();
+  }
 
 }
